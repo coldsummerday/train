6260fa76c8f7aaf7bfad02d3661887c5695a2b52
zhou <2808972009@qq.com>
2017-07-15 15:59:18 +0800

    3

--- testtcp.py
+++ testtcp.py
@@ -6,22 +6,20 @@
 import time, socket, threading
 
 def tcplink(sock, addr):
-    print 'Accept new connection from %s:%s...' % addr
-    sock.send('Welcome!')
     while True:
         data = sock.recv(1024)
         time.sleep(1)
         if data == 'exit' or not data:
             break
-        sock.send('Hello, %s!' % data)
+        print(data)
     sock.close()
-    print 'Connection from %s:%s closed.' % addr
+    
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 监听端口:
 s.bind(('192.168.199.154', 9999))
 s.listen(5)
-print 'Waiting for connection...'
+
 while True:
     # 接受一个新连接:
     sock, addr = s.accept()
