c5a3c1e974ad09342581eabad641059e91fb2b6a
zhou <2808972009@qq.com>
2017-07-15 15:53:23 +0800

    1

--- client.py
+++ client.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'a socket example which send echo message to server.'
+
 import socket
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-
-s.connect(('192.168.199.154',9999))
-while True:
-    data=raw_input("please input data you want to send \n")
-    if data!='close':
-        s.send(data)
-    else:
-        break
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接:
+s.connect(('192.168.199.154', 9999))
+# 接收欢迎消息:
+print s.recv(1024)
+for data in ['Michael', 'Tracy', 'Sarah']:
+    # 发送数据:
+    s.send(data)
+    print s.recv(1024)
+s.send('exit')
 s.close()
-        
